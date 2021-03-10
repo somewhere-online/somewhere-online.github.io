f8ab634db122f5d42b178abe9e120b43cd53dddc
jonathanxuexue <59134039+jonathanxuexue@users.noreply.github.com>
2021-03-10 12:46:02 -0800

    SITE READY

--- js/sketch.js
+++ js/sketch.js
@@ -1,77 +1,145 @@
 var canvas;
-var x;
-var y;
-var r, g, b;
-var w, h;
 var interval; 
 var counter = 0;
+var clicked = 0;
+var poly = [];
+hit = false;
+var textW;
+var textContent;
+
+function FocusOnInput()
+{
+     document.getElementById("InputID").focus();
+}
+
+function windowResized() {
+}
 
 function setup () {
-    canvas = createCanvas(400, 400);
-    x = (windowWidth - width) / 2;
-    y = (windowHeight - height) / 2;
-    canvas.position(x,y);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
     canvas.style('z-index', '-1')
     background('white');
 
-    inp = createInput();
-    inp.position(x + canvas.width/2 - inp.width/2 ,y + canvas.height/2 - inp.height/2);
+    inp = createInput().attribute('maxlength', 15)
+    inp.position(85, 90);
     inp.input(inputEvent);
+    inp.style('background-color', 'black');
+    inp.style('font-size', '32px');
+    inp.style('color', 'white');
+    inp.style('width', '200px');
+    inp.id('InputID');
+   
 
-    button = createButton('Go');
-    button.position(x + canvas.width/2 - button.width/2 ,y + canvas.height/2 + inp.height/2 + 5);
-    button.mousePressed(indefSomewhere);
+    // button.mousePressed(indefSomewhere);
+    poly[0] = createVector(80, 50);
+    poly[1] = createVector(300, 50)
+    poly[2] = createVector(350, 95)
+    poly[3] = createVector(300, 140)
+    poly[4] = createVector(80, 140)
+    fill(0);
+    beginShape();
+    for (const { x, y } of poly)  vertex(x, y);
+    endShape(CLOSE);
+
 }
 
 function inputEvent() {
     console.log(this.value())
 }
 
-function goSomewhere() {
+
+
+// function goSomewhere() {
+//     const place = inp.value();
+//     // for (let i = 0; i < 20; i++) {
+//         push();
+//         fill(random(255), random(255), random(255));
+//         translate(random(windowWidth), random(windowHeight));
+//         rotate(random(2*PI));
+//         noStroke();
+//         beginShape();
+    
+//         vertex(80, 50);
+//         vertex(300, 50);
+//         vertex(350, 95);
+//         vertex(300, 140);
+//         vertex(80, 140);
+//         scale(0.5);
+//         endShape(CLOSE);
+//         push();
+//         fill(random(255), random(255), random(255));
+//         text(place, 100, 124);
+//         pop();
+//         pop();
+//         counter++;
+//         console.log(counter);
+//         if(counter >= 5) {
+//             clearInterval(interval);
+//             counter = 0;
+//             console.log(counter);
+//         }
+//     //   }
+// }
+
+function drawSomewhere() {
     const place = inp.value();
+    textContent = inp.value();
+    textW = textWidth(textContent);
+    if(textW > textWidth('Somewhere')) {
+        textW =  textW * 1.4;
+        console.log(textW)
+    } else if (textW === textWidth('WWWWWWWWWWWWWW')){
+        textW =  textW * 1.6;
+    } else {
+        textW = 0;
+        console.log(textW)
+    }
+    
     // for (let i = 0; i < 20; i++) {
         push();
         fill(random(255), random(255), random(255));
         translate(random(width), random(height));
         rotate(random(2*PI));
+        noStroke();
         beginShape();
         vertex(80, 50);
-        vertex(300, 50);
-        vertex(350, 95);
-        vertex(300, 140);
+        vertex((300 + textW), 50);
+        vertex((350 + textW), 95);
+        vertex((300 + textW), 140);
         vertex(80, 140);
         scale(0.5);
         endShape(CLOSE);
         push();
+        textSize(32);
         fill(random(255), random(255), random(255));
         text(place, 100, 124);
         pop();
         pop();
-        counter++;
-        if(counter >= 5) {
-            clearInterval(interval);
-            counter = 0;
-        }
-    //   }
 }
 
-function indefSomewhere() {
-    interval = setInterval(goSomewhere, 100);
-}
+// function indefSomewhere() {
+//     clearInterval(interval);
+//     interval = setInterval(goSomewhere, 100);
+// }
 
 
 function draw() {
-    fill(0);
+    frameRate(50);
+    // fill(255);
+    // if(inp.value() != null) {
+    //     textSize(32);
+    //     text(inp.value(), 87.5, 125);
+    // }
     beginShape();
-    vertex(80, 50);
-    vertex(300, 50);
-    vertex(350, 95);
-    vertex(300, 140);
-    vertex(80, 140);
+    for (const { x, y } of poly)  vertex(x, y);
     endShape(CLOSE);
-    fill(255);
-    if(inp.value() != null) {
-        textSize(32);
-        text(inp.value(), 87.5, 125);
+    hit = collidePointPoly(mouseX, mouseY, poly);
+    if(hit && mouseIsPressed) {
+        setTimeout(drawSomewhere, 100);
     }
+    textContent = inp.value();
+    textW = textWidth(textContent);
+    console.log(textW);
 }
+
